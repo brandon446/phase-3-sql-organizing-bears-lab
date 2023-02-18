3167c8b606a59970bac1ad73bba441a87579bbaa
brandon <brandon.ngongolo@student.moringaschool.com>
2023-02-18 17:17:53 -0500

    Complete

---
+++ lib/create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE bears(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex INTEGER,
+    color INTEGER,
+    temperament INTEGER,
+    alive BOOLEAN
+
+)
+    
+
+
+
+
+
+
+
+
